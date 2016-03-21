9678024aeb53b261c88f0ad5581e30cdfc467b37
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-03-22 00:28:50 +0900

    initial commit

---
+++ TimerQuiz/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TimerQuiz
+//
+//  Created by 酒井文也 on 2016/03/17.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
