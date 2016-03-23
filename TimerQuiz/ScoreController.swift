ad619c08c8d5d40587dbd471210f87a544136486
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-03-24 02:16:59 +0900

    事前準備とRealm導入

---
+++ TimerQuiz/ScoreController.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreController.swift
+//  TimerQuiz
+//
+//  Created by 酒井文也 on 2016/03/24.
+//  Copyright © 2016年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ScoreController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
